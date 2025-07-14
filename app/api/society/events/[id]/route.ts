415082c45ab7c1f9e6f62cd2cbf7849246afee8c
Harsh <jatoliyaharsh8@gmail.com>
2025-07-14 14:13:33 +0530

    updated and added events

--- app/api/society/events/[id]/route.ts
+++ app/api/society/events/[id]/route.ts
@@ -1,6 +1,7 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(
+  req: NextRequest,
   context: { params: { id: string } }
 ) {
   const { id } = context.params;
